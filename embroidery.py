76cfbbf62d9f9a29a1d1abe8eeba3db273566a5f
Łukasz <Kdziora>
2020-01-07 00:02:09 +0100

    corrected value names/usr/bin/python3 /home/adrem01/Pulpit/projekty/code_cool/VSC/embroidery-Radek-Natalia-Lukasz/embroidery.py

--- embroidery.py
+++ embroidery.py
@@ -4,8 +4,8 @@ def draw_rectangle(width, height, border=0):
     i = 0
     inside_content = 2
     b = 0
-    insert_width = width - border*2
-    insert_heigh = height - border*2
+    inside_width = width - border*2
+    inside_heigh = height - border*2
     while i != height:
         while b != width:
             matrix.append(1)
@@ -13,9 +13,9 @@ def draw_rectangle(width, height, border=0):
         i = i + 1
         matrix1.append(matrix[:])
     if border > 0:
-        for i in range(insert_heigh):
+        for i in range(inside_heigh):
             f = 0
-            while f != insert_width:
+            while f != inside_width:
                 matrix1[border+i][border+f] = inside_content
                 f = f+1
     return matrix1
@@ -31,4 +31,4 @@ def embroider(matrix, color_scheme):
 
 if __name__ == '__main__':
     color_scheme = {0: ' ', 1: '*', 2: '.'}
-    embroider(draw_rectangle(100, 100, 10), color_scheme)
+    embroider(draw_rectangle(50, 50, 10), color_scheme)
